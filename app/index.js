7f546ef6090cf921e8673bc1c6e7961e421cc7e8
Greg <gdbabbert@gmail.com>
2023-06-07 19:02:49 -0400

    added comment

--- app/index.js
+++ app/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js'
 
+// This is where app is called
+
 ReactDOM.render(
   <App/>,
   document.getElementById('app') // make sure this is the same as the id of the div in your index.html
